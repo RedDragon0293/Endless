011986118a5fc057ea11fe995bf003b14bb7fbaf
RedDragon0293 <1352003034@qq.com>
2022-07-20 19:37:07 +0800

    removed

--- src/main/kotlin/cn/asone/endless/Endless.kt
+++ src/main/kotlin/cn/asone/endless/Endless.kt
@@ -7,13 +7,10 @@ import cn.asone.endless.features.module.ModuleManager
 import cn.asone.endless.script.ScriptManager
 import cn.asone.endless.ui.font.Fonts
 import cn.asone.endless.ui.gui.clickgui.GuiClickGUI
-import com.google.common.util.concurrent.ThreadFactoryBuilder
 import net.minecraft.client.main.Main
 import org.apache.logging.log4j.LogManager
 import org.lwjgl.opengl.Display
 import viamcp.ViaMCP
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.Executors
 
 object Endless {
     const val CLIENT_NAME = "Endless"
@@ -43,55 +40,31 @@ object Endless {
 
     fun startClient() {
         logger.info("正在启动 $CLIENT_NAME $CLIENT_VERSION...")
-        val i = System.currentTimeMillis()
-        val factory = ThreadFactoryBuilder()
-            .setDaemon(true)
-            .setNameFormat("Endless init thread #%d")
-            .build()
-        val asyncExec = Executors.newFixedThreadPool(5, factory)
-        val viaFuture = CompletableFuture.runAsync({
-            if (!disableVia) {
-                try {
-                    logger.info("正在加载 ViaVersion...")
-                    ViaMCP.getInstance()!!.start()
-                    ViaMCP.getInstance()!!.initAsyncSlider(120, 8, 110, 20)
-                } catch (e: Exception) {
-                    logger.error("无法加载 ViaVersion!")
-                    e.printStackTrace()
-                }
+        if (!disableVia) {
+            try {
+                logger.info("正在加载 ViaVersion...")
+                ViaMCP.getInstance()!!.start()
+                ViaMCP.getInstance()!!.initAsyncSlider(120, 8, 110, 20)
+            } catch (e: Exception) {
+                logger.error("无法加载 ViaVersion!")
+                e.printStackTrace()
             }
-        }, asyncExec)
-
-        val initFuture = CompletableFuture.runAsync({
-            ConfigManager
-            EventManager
-        }, asyncExec).thenRunAsync({
-            CommandManager
-            ModuleManager
-            EventManager.sort()
-        }, asyncExec)
-        val scriptFuture = CompletableFuture.runAsync({
-            ScriptManager.loadAllScripts()
-        }, asyncExec)
-        val configLoadingFuture = CompletableFuture.runAsync({
-            ConfigManager.loadAllConfigs()
-        }, asyncExec)
-        val clickGUIFuture = CompletableFuture.runAsync({
-            clickGUI = GuiClickGUI()
-        }, asyncExec)
-
+        }
+        ConfigManager
+        EventManager
+        CommandManager
+        ModuleManager
+        EventManager.sort()
+        ScriptManager.loadAllScripts()
+        ConfigManager.loadAllConfigs()
         Fonts.downloadFonts()
         Fonts.loadFonts()
-
-        while (!(initFuture.isDone && scriptFuture.isDone && configLoadingFuture.isDone && clickGUIFuture.isDone && viaFuture.isDone)) {
-            Thread.sleep(100L)
-        }
+        clickGUI = GuiClickGUI()
 
         Display.setTitle("$CLIENT_NAME $CLIENT_VERSION | 1.8.9 - Cracked by AsOne & RedDragon0293")
+        logger.info("成功加载 $CLIENT_NAME!")
         inited = true
-        asyncExec.shutdown()
         System.gc()
-        logger.info("成功加载 $CLIENT_NAME, 用时 ${(System.currentTimeMillis() - i) / 1000F}秒.")
     }
 
     fun stopClient() {
