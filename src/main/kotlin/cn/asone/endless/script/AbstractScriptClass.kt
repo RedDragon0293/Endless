5c9bc01417b3904f861f2b6266cb01caa044652f
RedDragon <1352003034@qq.com>
2023-07-31 16:53:42 +0800

    忘记改了

--- src/main/kotlin/cn/asone/endless/script/AbstractScriptClass.kt
+++ src/main/kotlin/cn/asone/endless/script/AbstractScriptClass.kt
@@ -1,8 +1,6 @@
 package cn.asone.endless.script
 
-import cn.asone.endless.event.EventHook
 import cn.asone.endless.features.module.AbstractModule
-import cn.asone.endless.option.AbstractOption
 
 abstract class AbstractScriptClass(
     name: String,
@@ -14,6 +12,4 @@ abstract class AbstractScriptClass(
     abstract val scriptName: String
     abstract val scriptVersion: String
     abstract val scriptAuthor: String
-    override val handledEvents: ArrayList<EventHook> = arrayListOf()
-    override val options: ArrayList<AbstractOption<*>> = arrayListOf()
 }
